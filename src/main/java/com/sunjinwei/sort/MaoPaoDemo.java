a31b15c0cd1e97228a0608db5824ea3c458b7cf9
sunjinwei <sunjinwei5828@163.com>
2020-06-30 16:51:43 +0800

    opt: 使用自己的方式实现冒泡排序代码-while循环代替外层循环

--- src/main/java/com/sunjinwei/sort/MaoPaoDemo.java
+++ src/main/java/com/sunjinwei/sort/MaoPaoDemo.java
@@ -13,21 +13,33 @@ public class MaoPaoDemo {
 
     public static void main(String[] args) {
         // 长度为5的数组 因为数组索引是从0开始 那么只需要n-1次外循环 内循环就是n-1-1次
-        Integer[] arr = {3, 1, 10, 7, 4};
+        Integer[] arr01 = {3, 1, 10, 7, 4, 3, 9, 2};
 
-        System.out.println(arr.length);
+        System.out.println("数组长度为：" + arr01.length);
 
-        MaoPaoDemo.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        sort01(arr01);
+
+        System.out.println("双重for循环冒泡排序后的数组为：" + Arrays.toString(arr01));
+
+        System.out.println("======================");
+
+        Integer[] arr02 = {3, 1, 10, 7, 4, 3, 9, 2};
+
+        System.out.println("数组长度为：" + arr02.length);
+
+        sort02(arr02);
+
+        System.out.println("while循环冒泡排序后的数组为：" + Arrays.toString(arr02));
 
     }
 
     /**
+     * 方法一：
      * 进行冒泡排序
      *
      * @param arr
      */
-    public static void sort(Integer[] arr) {
+    public static void sort01(Integer[] arr) {
         // 外层循环 控制趟数 length-1次
         for (int i = 0; i < arr.length - 1; i++) {
             // 内层循环 因为每次都会将最大的数排在最右边 所以第二趟都会比第一趟少一次 第三趟会比第二趟少一次
@@ -39,9 +51,28 @@ public class MaoPaoDemo {
         }
     }
 
+    /**
+     * 方法二：使用while循环代替外层循环 更好理解
+     *
+     * @param arr
+     */
+    public static void sort02(Integer[] arr) {
+        // 最后一个无需进行比较 所以while循环 如果只剩下最后一个元素 那么无需进行比较 跳出循环
+        int length = arr.length;
+        while (length > 1) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (handlerMinAndMax(arr[i], arr[i + 1])) {
+                    exchangeIndex(arr, i, i + 1);
+                }
+            }
+            length--;
+        }
+    }
+
 
     /**
      * 进行比较两个数大小
+     * 如果a>b 为true；如果a<b 为false
      *
      * @param a
      * @param b
